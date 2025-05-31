3733ed496bbf1f526414ad935afcc28ba0a01de2
Nilesh Niraula <140038544+niraulanilesh7@users.noreply.github.com>
2025-05-31 10:36:51 +0000

    operations in different datatypes

---
+++ 01_basics/03.1_ConversionOperation.js
@@ -0,0 +1,32 @@
+// Operations
+
+let value=3
+let negValue= -value
+console.log(negValue)
+
+console.log(2**2)// 2**2 gives the value 2 power raised to 2
+console.log(2%3) // it gives the remainder 
+
+let str1="hello"
+let str2=" nilesh"
+let str3= str1 + str2
+
+// console.log(str3)
+// console.log('1'+2)
+// console.log(1+"2")
+// console.log('1'+2+2)
+// console.log(1+2+"2")
+
+let newTrue = +true
+console.log(typeof newTrue)
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3  
+num1=num2=num3=2+2;//Not a good practise for production
+
+let gameCounter= 100;
+gameCounter ++ // increment operator
+console.log(gameCounter)
+
