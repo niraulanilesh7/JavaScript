a654256c9a49fd9176b7d2df22c607a7db8a7fa2
Nilesh Niraula <140038544+niraulanilesh7@users.noreply.github.com>
2025-05-31 08:46:54 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score=true
+//you can use shift + alt + downarrow to copy the selected text in next line
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+
+console.log(valueInNumber)
+
+
+//"33"=> 33
+// "33abc"=> NaN    note: type of nan is also a number
+// true=> 1  false=> 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// if we use ""--> false, "Nilesh"-->true
+// 1-> true  0 --> false
+
+let someNumber= 33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
