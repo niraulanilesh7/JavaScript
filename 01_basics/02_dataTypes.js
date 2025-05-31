0e6a0db46928835d8c362f88386745601c142c6d
Nilesh Niraula <140038544+niraulanilesh7@users.noreply.github.com>
2025-05-31 08:28:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js code as newer version
+
+//alert("hello") //we are using nodejs,not browser
+
+console.log("Nilesh")
+console.log(3+
+    3) // code readability should be high
+
+let name="nilesh"
+let age=18
+let isLoggedIn= true
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null =>standalone value
+// undefined => the value has not been assigned yet
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)//object
+
