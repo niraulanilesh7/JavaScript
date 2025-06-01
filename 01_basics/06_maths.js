36b263158361ce00d2868354880194aa4ae5686d
Nilesh Niraula <140038544+niraulanilesh7@users.noreply.github.com>
2025-06-01 08:18:35 +0000

    Maths in JS

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,21 @@
+//Maths
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(44.6342));//roundoff before the point ie 45 only not .
+console.log(Math.ceil(4.2));// gives the top valueof interger ie 5
+console.log(Math.floor(4.2));// gives the bottom valueof interger ie 4
+
+console.log(Math.sqrt(4))// gives squareroot ie 2
+console.log(Math.pow(4,4))// gives power of first argument
+console.log(Math.min(4,3,5,6,7,1))// gives the min value of arrays
+console.log(Math.max(4,3,5,6,7,1))// gives the max value of arrays
+console.log(Math.random());//generates random values between 0 and 1
+console.log((Math.random()*10)+1);//since value can be 0.041..so to avoid 0,add 1
+console.log(Math.floor((Math.random()*10)+1));
+
+const min =10
+const max=20
+
+console.log(Math.round(Math.random()*(max-min))+min);
+
