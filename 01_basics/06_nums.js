6230771e6798cb1c2a64d3dfa5fe21ca607e4703
Nilesh Niraula <140038544+niraulanilesh7@users.noreply.github.com>
2025-06-01 07:48:41 +0000

    Numbers

---
+++ 01_basics/06_nums.js
@@ -0,0 +1,31 @@
+const score=400;
+console.log(score)
+
+const balance= new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length) /* change number to string and you can use string properties */
+
+console.log(balance.toFixed(2))// to fix decimal places after points
+
+const otherNumber= 123.8966
+console.log(otherNumber.toPrecision(3))//it roundoff the last digit
+// precision takes the decimal place from the MSB not points
+
+const hundreds= 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+hundreds.
+
+/*
+MAX_SAFE_INTEGER: 9007199254740991
+​
+MAX_VALUE: 1.7976931348623157e+308
+​
+MIN_SAFE_INTEGER: -9007199254740991
+​
+MIN_VALUE: 5e-324
+*/
+
+
+
